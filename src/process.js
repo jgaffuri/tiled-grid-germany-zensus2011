8a2bac9965fe9a0cec79deae3769f46f1a563cc1
jgaffuri <jgaf@proton.me>
2023-08-29 13:02:22 +0200

    a

--- src/process.js
+++ src/process.js
@@ -1,4 +1,14 @@
+#!/usr/bin/env node
 
 
+//See https://observablehq.com/d/62acd402b96476b9
+//https://uwdata.github.io/arquero/api/
+
+//import {aq, op} from "arquero"
+const aq = require("arquero");
 
 console.log("Hallo welt !")
+console.log(aq.loadCSV)
+
+const dt = aq.loadCSV('./input/csv_Demographie_100m_Gitter/Bevoelkerung100M.csv');
+//2GB limit exceeded !!!
