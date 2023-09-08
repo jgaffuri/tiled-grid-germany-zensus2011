150b57377520d156654551e29dc61878085c68e5
jgaffuri <jgaf@proton.me>
2023-09-08 14:43:18 +0200

    a

--- src/tiling.py
+++ src/tiling.py
@@ -3,7 +3,7 @@ import subprocess
 # /home/juju/pythonvenvgridDE/bin/python ./src/tiling.py /usr/bin/python3 /home/juju/workspace/tiled-grid-germany-zensus2011/src/tiling.py
 
 
-def tiling(code, a):
+def tiling(code, a, t):
     print(code + " " + str(a * 100) + "m")
 
     # gridtiler -i ./input/out_ALTER_KURZ.csv -r 100 -c 3035 -x 3900000 -y 2600000 -p "const a = c.grd_id.split('N')[1].split('E'); return { x:100*a[1],y:100*a[0] };" -m "delete c.grd_id" -a 1 -o ./out/ALTER_KURZcsv/100m/ -e csv
@@ -29,6 +29,8 @@ def tiling(code, a):
             str(a),
             "-o",
             "./out/" + code + "csv/" + str(a * 100) + "m/",
+            "-t",
+            str(t),
             "-e",
             "csv",
         ]
@@ -48,4 +50,5 @@ for code in [
     "HEIZTYP",
 ]:
     for a in [1, 2, 5, 10, 20, 50, 100, 200, 500]:
-        tiling(code, a)
+        t = 256 if code == "INSGESAMT" else 128
+        tiling(code, a, t)
