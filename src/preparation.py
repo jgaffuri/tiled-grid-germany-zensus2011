f1ff0b702a4d70150ccf5d4b200772bde48b5ac1
jgaffuri <jgaf@proton.me>
2023-09-08 14:23:05 +0200

    a

--- src/preparation.py
+++ src/preparation.py
@@ -12,21 +12,18 @@ def prepare(csvfile, sep, code, printfinal):
     print("Load data")
     df = pd.read_csv(csvfile, sep=sep, encoding="iso-8859-1") #, nrows=10000)
 
-    print("drop unecessary columns")
-    df = df.drop(["Gitter_ID_100m_neu", "Auspraegung_Text", "Anzahl_q"], axis=1)
-
     print("drop unecessary rows")
     df = df[df.Merkmal == code]
 
-    print("Drop unecessary columns")
-    df = df.drop(["Merkmal"], axis=1)
+    print("drop unecessary columns")
+    df = df.drop(["Gitter_ID_100m_neu", "Auspraegung_Text", "Anzahl_q", "Merkmal"], axis=1)
 
     print("Make new grd_id column")
     df["grd_id"] = df.apply(
         lambda row: row["Gitter_ID_100m"].replace("100m", ""), axis=1
     )
 
-    print("Drop unecessary column")
+    print("Drop unecessary column Gitter_ID_100m")
     df = df.drop(["Gitter_ID_100m"], axis=1)
 
     print("Rename Anzahl column")
@@ -41,9 +38,6 @@ def prepare(csvfile, sep, code, printfinal):
         aggfunc=np.sum,
         fill_value=0,
     )
-
-    # print("Round")
-    # df = df.round()
 
     if printfinal:
         print(df)
--- src/tiling.py
+++ src/tiling.py
@@ -1,15 +1,51 @@
 import subprocess
 
+# /home/juju/pythonvenvgridDE/bin/python ./src/tiling.py /usr/bin/python3 /home/juju/workspace/tiled-grid-germany-zensus2011/src/tiling.py
 
-codes = [" INSGESAMT","ALTER_KURZ","FAMSTND_AUSF","GEBURTLAND_GRP","GESCHLECHT","RELIGION_KURZ","STAATSANGE_GRP","GEBTYPBAUWEISE","BAUJAHR_MZ","HEIZTYP"]
+codes = [
+    " INSGESAMT",
+    "ALTER_KURZ",
+    "FAMSTND_AUSF",
+    "GEBURTLAND_GRP",
+    "GESCHLECHT",
+    "RELIGION_KURZ",
+    "STAATSANGE_GRP",
+    "GEBTYPBAUWEISE",
+    "BAUJAHR_MZ",
+    "HEIZTYP",
+]
 
 
+def tiling(code, a):
+    print(code + str(a * 100) + "m")
+
+# gridtiler -i ./input/out_ALTER_KURZ.csv -r 100 -c 3035 -x 3900000 -y 2600000 -p "const a=c.grd_id.split('N')[1].split('E');return {x:100*a[1],y:100*a[0]};" -m "delete c.grd_id" -a 1 -o ./out/ALTER_KURZcsv/100m/ -e csv
+
+    subprocess.run(
+        [
+            "gridtiler",
+            "-i",
+            "./input/out_" + code + ".csv",
+            "-r",
+            "100",
+            "-c",
+            "3035",
+            "-x",
+            "3900000",
+            "-y",
+            "2600000",
+            "-p",
+            "\"const a=c.grd_id.split('N')[1].split('E');return {x:100*a[1],y:100*a[0]};\"",
+            "-m",
+            '"delete c.grd_id"',
+            "-a",
+            str(a),
+            "-o",
+            "./out/" + code + "csv/" + str(a * 100) + "m/",
+            "-e",
+            "csv",
+        ]
+    )
 
 
-def tiling(code):
-    print(code)
-
-    # -i ./input/csv_Demographie_100m_Gitter/out_demo.csv -r 100 -c 3035 -x 3900000 -y 2600000 -p "const a=c.grd_id.split('N')[1].split('E');return {x:100*a[1],y:100*a[0]};" -m "delete c.grd_id" -a 1 -o ./out/demo/100m/ -e parquet
-
-    subprocess.run(["gridtiler", "-i", "./dddd/"])
-
+tiling("ALTER_KURZ", 1)
